b49942c909c7a5a0b90b065b0bf76784e29e3916
Dark <soumyadwip@outlook.in>
2022-02-19 21:49:20 +0530

    hehe

--- exploWebApp/settings.py
+++ exploWebApp/settings.py
@@ -160,3 +160,19 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
