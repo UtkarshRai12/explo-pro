9537eea11e017e2acd274c82216e9444a67c975c
Dark <soumyadwip@outlook.in>
2022-02-19 18:08:22 +0530

    whitenoise

--- exploWebApp/settings.py
+++ exploWebApp/settings.py
@@ -60,6 +60,7 @@ SWAGGER_SETTINGS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
